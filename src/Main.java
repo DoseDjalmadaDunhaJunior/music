76311ea55f2057836c7366d0cc048b97e286a266
djalma <djalminhajr_cunha@hotmail.com>
2019-04-08 19:51:26 -0300

    um import util para ser agregado

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+/**
+ * esse import vai ser util para analizar arquivos musicais e fazer grafico
+ */
+
+import java.awt.Graphics2D;
 public class Main {
 
     public static void main(String[] args) {
