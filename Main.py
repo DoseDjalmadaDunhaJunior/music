7cf9b35541d295926ef17dbfeb38f83c09f3f61d
josé djalma da cunha júnior <josedjalma@MacBook-Air-de-jose.local>
2019-04-22 16:13:51 -0300

    coloquei uma musica simplismente para testar, porem tentei com as musicas no formato .au do arquivo e com esse da erro

--- Main.py
+++ Main.py
@@ -1,21 +1,5 @@
-# Importando o PyGame
 import pygame
-import os
-
-# Inicializando o PyGame
 pygame.init()
-
-# Carregando o arquivo MP3 e executando
-if os.path.exists('Psicose.mp3'):
-    pygame.mixer.music.load('Psicose.mp3')
-    pygame.mixer.music.play()
-    pygame.mixer.music.set_volume(1)
-
-    clock = pygame.time.Clock()
-    clock.tick(10)
-
-    while pygame.mixer.music.get_busy():
-        pygame.event.poll()
-        clock.tick(10)
-else:
-    print('O arquivo musica.mp3 não está no diretório do script Python')
+pygame.mixer.music.load("Psicose.mp3")
+pygame.mixer.music.play()
+pygame.event.wait()
