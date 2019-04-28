badf06d1ea1fa1ebeba047033fd693b73b4a6d84
josé djalma da cunha júnior <josedjalma@MacBook-Air-de-jose.local>
2019-04-28 12:09:36 -0300

    agora tem o TensorFlow

--- Main.py
+++ Main.py
@@ -2,6 +2,6 @@
 import pygame
 pygame.init()
 #pygame.mixer.music.load("rock.00000.au")
-pygame.mixer.music.load("Psicose.mp3")
+pygame.mixer.music.load("Peido.mp3")
 pygame.mixer.music.play()
 pygame.event.wait()
